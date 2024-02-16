f78c1776865ca2c26316238c77baff917e3e759a
vishalguptamonu <vishalkrmonu@gmail.com>
2024-02-16 13:49:25 +0530

    code

---
+++ day4/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+
+
+let name="vishal";
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <img src="" alt="" />
+      <div className="container">
+      <h1> hello {name}</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+          iste quisquam ea itaque impedit doloremque illo blanditiis voluptatum
+          veritatis dignissimos. Doloremque ipsa sunt sit odio! Iusto nihil
+          quidem autem hic quo! Laudantium cum inventore natus consectetur nisi
+          facere a labore!
+        </p>
+      </div>
+      <div className="blank">Lovely</div>
+    </>
+  );
+}
+
+export default App;
