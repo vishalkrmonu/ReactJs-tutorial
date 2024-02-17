13328b3333b2ed555b78af90ca98aad2146d3103
vishalguptamonu <vishalkrmonu@gmail.com>
2024-02-17 19:07:08 +0530

    code

--- day5/src/App.js
+++ day5/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -13,6 +14,8 @@ function App() {
 <Navbar title="TextUtils"/>
 <div className="container" my-3>
 <TextForm heading="Enter the text to analyze"/> 
+{/*  new page */}
+{/* <About/> */}
 </div>
 
     </>
--- day5/src/components/TextForm.js
+++ day5/src/components/TextForm.js
@@ -24,8 +24,17 @@ const handleOnChange= (event)=>{
   setText(event.target.value)
 }
 
+const handleCopy= ()=>{
+  var text=document.getElementById("myBox");
+  text.Select();
+  navigator.clipboard.writeText(text.value);
+}
 
 
+const handleExtraSpaces=()=>{
+  let newText=text.split(/[ ]+/);
+  setText(newText.join(" "))
+}
 
   //DEclare a new state variable which we will call count
   const [text, setText] = useState("Enter text here");
@@ -36,22 +45,24 @@ const handleOnChange= (event)=>{
   return (
     <>
     <div className="container">
-      <h1>{props.heading} </h1>
+      <h3>{props.heading} </h3>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-1" onClick={handleUpClick}> Convert to Uppercase</button>
       <button className="btn btn-primary mx-1" onClick={handleLoClick}> Convert to Lowercase</button>
       <button className="btn btn-primary mx-1" onClick={handleClearClick}> Clear</button>
+      <button className="btn btn-primary mx-1" onClick={handleCopy}> Copy Text</button>
+      <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}> Remove Extra Space</button>
 
 
     </div>
 
     <div className="container" my-3>
-      <h2>Your text summary</h2>
+      <h4>Your text summary</h4>
       <p>{text.split(' ').length} words and {text.length} characters</p>
       <p>{0.008*text.split(' ').length} Minutes read</p>
-      <h2>Preview</h2>
+      <h4>Preview</h4>
       <p>{text}</p>
     </div>
     </>
